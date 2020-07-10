dfadf417054568d83f47adb8f56f6a5df63f2afa
Andrey Anishchenko <andrew.anishchenko@gmail.com>
2020-07-10 10:15:38 +0200

    Added direct access client

---
+++ Direct.Client/CustomerServiceClient.cs
@@ -0,0 +1,36 @@
+﻿using So.Demo.Common.Requests;
+using So.Demo.Common.Responses;
+using So.Demo.Common.Services;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace So.Demo.Direct.Client
+{
+    /// <summary>
+    /// Direct access is supposed to access the db directly, so it just generates the customers and gives them back
+    /// </summary>
+    class CustomerServiceClient : ICustomerService
+    {
+        private readonly ICustomerFactory _customerFactory;
+
+        public CustomerServiceClient(ICustomerFactory customerFactory)
+        {
+            _customerFactory = customerFactory;
+        }
+
+        public string Name => "Direct Db access";
+
+        public Task<CustomersResponse> GetCustomersAsync(CustomersRequest request)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var customers = _customerFactory.CreateRandom(request.CustomersCount);
+            stopwatch.Stop();
+            return Task.FromResult(
+                new CustomersResponse
+                {
+                    Customers = customers,
+                    Duration = stopwatch.ElapsedMilliseconds
+                });
+        }
+    }
+}
---
+++ Direct.Client/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using So.Demo.Common;
+using So.Demo.Common.Services;
+
+namespace So.Demo.Direct.Client
+{
+    /// <summary>
+    /// Adds necessary dependencies to Dependency Injection.
+    /// </summary>
+    public class DependencyInjection : IServiceClientDI
+    {
+        public void RegisterClient(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<ICustomerFactory, CustomerFactory>();
+            services.AddSingleton<ICustomerService, CustomerServiceClient>();
+        }
+    }
+}
